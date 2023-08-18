35ffcaf7f3a95efb2b86de064575e17534105788
bagus491 <baguskalam491@gmail.com>
2023-08-18 11:12:50 +0700

    setup(app) ==> Setup(6) -- 6 ==> nav ejs

--- src/Router/UserRouter.js
+++ src/Router/UserRouter.js
@@ -11,6 +11,8 @@ app.use(mainlayouts)
 const path = require('path')
 app.set('views',path.join(__dirname, '../views'))
 
+app.use(express.static(path.join(__dirname, '../public/')))
+
 //get
 app.get('/',HomeWeb)
 
