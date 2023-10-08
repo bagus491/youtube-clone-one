aa43a07bbcc2a1b5760b3ebb33e8917f7f85cd7c
bagus491 <baguskalam491@gmail.com>
2023-10-08 10:00:38 +0700

    done Controller

--- src/Controllers/Auth/LogoutController.js
+++ src/Controllers/Auth/LogoutController.js
@@ -4,7 +4,7 @@ const LogoutUser = (req,res) => {
     try{
     req.flash('msg','success Logout')
     res.clearCookie('token','')
-    res.session.User = ''
+    req.session.User = ''
     res.redirect('/login')
     }catch(error){
         return res.status(500).send({msg:'Internal Server Error'})
